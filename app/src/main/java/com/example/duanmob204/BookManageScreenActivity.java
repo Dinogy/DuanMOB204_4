acd044dbe52855d9aaf46fbd10cf160c69e1d6b8
Dinogy <rinrin.production1@gmail.com>
2020-10-07 11:22:01 +0700

    add //2

--- app/src/main/java/com/example/duanmob204/BookManageScreenActivity.java
+++ app/src/main/java/com/example/duanmob204/BookManageScreenActivity.java
@@ -10,5 +10,6 @@ public class BookManageScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_manage_screen);
+        //
     }
 }
